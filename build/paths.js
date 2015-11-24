ef9051d899016b010785ec15ad2c1ea234b9a4cd
Marc Scheib <marc.scheib@gmx.de>
2015-11-24 23:17:27 +0100

    chore(build): add gulp tasks

---
+++ build/paths.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var fs = require('fs');
+
+var appRoot = 'src/';
+var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  style: 'styles/**/*.css',
+  less: appRoot + 'styles/**/*.less',
+  output: 'dist/',
+  sample: 'sample',
+  doc:'./doc',
+  e2eSpecsSrc: 'test/e2e/src/*.js',
+  e2eSpecsDist: 'test/e2e/dist/',
+  packageName: pkg.name,
+  styleFolder: './styles',
+};
