d9a5072f4999562a952fca00050b36ffb00e3bcc
Marc Scheib <marc.scheib@gmx.de>
2015-11-28 17:41:04 +0100

    feat(sample): add a sample sub project

---
+++ sample/src/app.js
@@ -0,0 +1,12 @@
+import {inject} from 'aurelia-framework';
+import {NotificationService} from 'aurelia-notification';
+
+@inject(NotificationService)
+export class App {
+  constructor(notificationService) {
+    this.notificationService = notificationService;
+  }
+  submit(){
+    this.notificationService.info("blub");
+  }
+}
