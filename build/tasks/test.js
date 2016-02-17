86de6402330aeb4ae26ddf91db1d5e554eff6b0b
Marc Scheib <marc.scheib@gmx.de>
2016-02-17 20:14:19 +0100

    chore(ci): set up travis and coveralls

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,40 +1,34 @@
 var gulp = require('gulp');
-var Karma = require('karma').Server;
+var coveralls = require('gulp-coveralls');
+var karma = require('karma');
 
 /**
  * Run test once and exit
  */
 gulp.task('test', function (done) {
-  new Karma({
+  new karma.Server({
     configFile: __dirname + '/../../karma.conf.js',
     singleRun: true
-  }, done).start();
+  }, function(e) {
+    done(e === 0 ? null : 'karma exited with status ' + e);
+  }).start();
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
 gulp.task('tdd', function (done) {
-  new Karma({
+  new karma.Server({
     configFile: __dirname + '/../../karma.conf.js'
-  }, done).start();
+  }, function(e) {
+    done();
+  }).start();
 });
 
 /**
- * Run test once with code coverage and exit
+ * Report coverage to coveralls
  */
-gulp.task('cover', function (done) {
-  new Karma({
-    configFile: __dirname + '/../../karma.conf.js',
-    singleRun: true,
-    reporters: ['coverage'],
-    preprocessors: {
-      'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel', 'coverage']
-    },
-    coverageReporter: {
-      type: 'html',
-      dir: 'build/reports/coverage'
-    }
-  }, done).start();
+gulp.task('coveralls', ['test'], function (done) {
+  gulp.src('build/reports/coverage/lcov/report-lcovonly.txt')
+    .pipe(coveralls());
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,81 +1,108 @@
-// Karma configuration
-// Generated on Fri Dec 05 2014 16:49:29 GMT-0500 (EST)
+var isparta = require('isparta');
+var paths = require('./build/paths');
+var babelOptions = require('./build/babel-options');
 
 module.exports = function(config) {
-  config.set({
-
+  var configuration = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jspm', 'jasmine'],
 
     jspm: {
-      // Edit this to your needs
-      loadFiles: ['src/**/*.js', 'test/**/*.js']
+      config: 'config.js',
+      loadFiles: [paths.tests, paths.source],
+      paths: {
+        '*': '*.js',
+        'github:*': 'jspm_packages/github/*',
+        'npm:*': 'jspm_packages/npm/*'
+      }
     },
-
 
     // list of files / patterns to load in the browser
     files: [],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel']
+      [paths.tests]: ['babel'],
+      [paths.source]: ['babel', 'sourcemap', 'coverage']
     },
-    'babelPreprocessor': {
+
+    babelPreprocessor: {
       options: {
-        sourceMap: 'inline',
-        modules: 'system',
-        moduleIds: false,
-        loose: "all",
-        optional: [
-          "es7.decorators",
-	        "es7.classProperties"
-        ]
+        loose: babelOptions.loose,
+        stage: babelOptions.stage,
+        optional: babelOptions.optional,
+        sourceMap: 'inline'
       }
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['coverage', 'progress'],
 
+    coverageReporter: {
+      instrumenters: {
+        isparta: isparta
+      },
+
+      instrumenter: {
+        [paths.source]: 'isparta'
+      },
+
+      dir: 'build/reports/coverage/',
+
+      reporters: [{
+        type: 'text-summary'
+      }, {
+        type: 'html',
+        subdir: 'html'
+      }, {
+        type: 'lcovonly',
+        subdir: 'lcov',
+        file: 'report-lcovonly.txt'
+      }]
+    },
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
+    browsers: ['Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
+  };
+
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
