e61a8ee01868e82acbb6efc17a68c6c55eeb7f13
Marc Scheib <marc.scheib@gmx.de>
2016-02-22 21:29:48 +0100

    chore(*): update task for changelog generation

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,30 +1,32 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var changelog = require('conventional-changelog');
+var changelog = require('gulp-conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
 
-gulp.task('bump-version', function(){
+gulp.task('bump-version', function () {
   return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
+    .pipe(bump({type: args.bump})) //major|minor|patch|prerelease
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('changelog', function(callback) {
+gulp.task('changelog', function () {
   var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: paths.doc + '/CHANGELOG.md'
-  }, function(err, log) {
-    fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
-  });
+  return gulp.src(paths.doc + '/CHANGELOG.md', {
+      buffer: false
+    })
+    .pipe(changelog({
+      preset: 'angular',
+      repository: pkg.repository.url,
+      version: pkg.version
+    }))
+    .pipe(gulp.dest(paths.doc + '/'));
 });
 
-gulp.task('prepare-release', function(callback){
+gulp.task('prepare-release', function (callback) {
   return runSequence(
     'build',
     'lint',
