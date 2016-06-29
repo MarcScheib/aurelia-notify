30ee1bab58f7a2635b0d6843b536ef0ad675aba5
Marc Scheib <marc.scheib@gmx.de>
2016-06-29 19:58:27 +0200

    fix(sample): add aurelia-pal

--- sample/config.js
+++ sample/config.js
@@ -28,6 +28,7 @@ System.config({
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-rc.1.0.0",
     "aurelia-framework": "npm:aurelia-framework@1.0.0-rc.1.0.0",
     "aurelia-metadata": "npm:aurelia-metadata@1.0.0-rc.1.0.0",
+    "aurelia-pal": "npm:aurelia-pal@1.0.0-rc.1.0.0",
     "aurelia-templating": "npm:aurelia-templating@1.0.0-rc.1.0.0",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
