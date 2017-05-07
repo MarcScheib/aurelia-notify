95f54157a2e11a87d029febda4d7853bd52f2c88
Marc Scheib <marc.scheib@gmx.de>
2017-05-07 16:48:54 +0200

    test(lifecycle): improve testcase

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,17 +1,14 @@
-export function invokeLifecycle(instance: any, name: string, model: any) {
+export function invokeLifecycle(instance: any, name: string, model?: any): Promise<any> {
   if (typeof instance[name] === 'function') {
-    let result = instance[name](model);
-
-    if (result instanceof Promise) {
-      return result;
-    }
-
-    if (result !== null && result !== undefined) {
-      return Promise.resolve(result);
-    }
-
-    return Promise.resolve(true);
+    return new Promise(resolve => {
+        resolve(instance[name](model))
+      })
+      .then(result => {
+        if (result !== null && result !== undefined) {
+          return result;
+        }
+        return true;
+      });
   }
-
   return Promise.resolve(true);
 }
--- test/unit/lifecycle.spec.js
+++ test/unit/lifecycle.spec.js
@@ -1,61 +1,94 @@
 import {invokeLifecycle} from '../../src/lifecycle';
 
-class ViewModelStub {
-  param = '';
+describe('invokeLifecycle', () => {
+  const DEFAULT_LIFECYCLE_RESULT = true;
+  const DEFAULT_INVOCATION_METHOD_NAME = 'canActivate';
+  const VM = {
+    [DEFAULT_INVOCATION_METHOD_NAME]() {
+      return this.output
+    }
+  };
 
-  activatePromise() {
-    return Promise.resolve('promise');
+  function testResult(done, output, expected = DEFAULT_LIFECYCLE_RESULT, methodName = DEFAULT_INVOCATION_METHOD_NAME) {
+    VM.output = output;
+    invokeLifecycle(VM, methodName)
+      .then(result => {
+        expect(result).toBe(expected);
+        done();
+      });
   }
 
-  activateNonPromise() {
-    return 'no promise';
+  function testRejection(done, expected) {
+    invokeLifecycle(VM, DEFAULT_INVOCATION_METHOD_NAME)
+      .then(() => {
+        done.fail('Rejection expected');
+      })
+      .catch(error => {
+        expect(error).toBe(expected);
+        done();
+      });
   }
-
-  activateNonReturn() {
-  }
-}
-
-describe('the lifecycle util', () => {
-  let instance;
-  let model = {};
 
   beforeEach(() => {
-    instance = new ViewModelStub();
+    delete VM.output;
   });
 
-  it('should return the promise of the invocation', () => {
-    let result = invokeLifecycle(instance, 'activatePromise', model);
+  describe('should resolve with default value if the invoked method', () => {
+    it('is not implemented', done => {
+      testResult(done, undefined, DEFAULT_LIFECYCLE_RESULT, 'deactivate');
+    });
 
-    expect(result instanceof Promise).toBe(true);
-    result.then(result => {
-      expect(result).toEqual('promise');
+    it('returns "undefined"', done => {
+      testResult(done, undefined);
+    });
+
+    it('returns "null"', done => {
+      testResult(done, null);
+    });
+
+    it('resolves to "undefined"', done => {
+      testResult(done, Promise.resolve(undefined));
+    });
+
+    it('resolves to "null"', done => {
+      testResult(done, Promise.resolve(null));
     });
   });
 
-  it('should return a resolved promise if function does not return one', () => {
-    let result = invokeLifecycle(instance, 'activateNonPromise', model);
-
-    expect(result instanceof Promise).toBe(true);
-    result.then(result => {
-      expect(result).toEqual('no promise');
-    });
+  it('resolves with the returned value', done => {
+    let expected = 'returned_value';
+    testResult(done, expected, expected);
   });
 
-  it('should return a resolved promise if function returns nothing', () => {
-    let result = invokeLifecycle(instance, 'activateNonReturn', model);
-
-    expect(result instanceof Promise).toBe(true);
-    result.then(result => {
-      expect(result).toBe(true);
-    });
+  it('resolves with the resolved value', done => {
+    let expected = 'resolved_value';
+    testResult(done, Promise.resolve(expected), expected);
   });
 
-  it('should return a resolved promise if no function is given', () => {
-    let result = invokeLifecycle(instance, 'param', model);
+  it('propagates errors when the invoked method throws', done => {
+    let expectedError = new Error('sync_error');
+    spyOn(VM, DEFAULT_INVOCATION_METHOD_NAME).and.callFake(() => { throw expectedError; });
+    testRejection(done, expectedError);
+  });
 
-    expect(result instanceof Promise).toBe(true);
-    result.then(result => {
-      expect(result).toBe(true);
-    });
+  it('propagates errors when the invoked method is rejected', done => {
+    let expectedError = new Error();
+    spyOn(VM, DEFAULT_INVOCATION_METHOD_NAME).and.returnValue(Promise.reject(expectedError));
+    testRejection(done, expectedError);
+  });
+
+  it('invokes the method with the provided model', done => {
+    let expectedModel = { test: 'test model' };
+    spyOn(VM, DEFAULT_INVOCATION_METHOD_NAME);
+    invokeLifecycle(VM, DEFAULT_INVOCATION_METHOD_NAME, expectedModel)
+      .then(result => {
+        expect(result).toEqual(DEFAULT_LIFECYCLE_RESULT);
+        done();
+      })
+      .catch(error => {
+        done.fail(error);
+      });
+
+    expect(VM[DEFAULT_INVOCATION_METHOD_NAME]).toHaveBeenCalledWith(expectedModel);
   });
 });
