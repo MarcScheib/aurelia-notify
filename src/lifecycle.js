3fdd56f20238e849909ab48f0f9dddcf4a776dd9
Marc Scheib <marc.scheib@gmx.de>
2017-05-15 18:37:16 +0200

    style(code): fix indentation and missing semicolon

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,8 +1,8 @@
 export function invokeLifecycle(instance: any, name: string, model?: any): Promise<any> {
   if (typeof instance[name] === 'function') {
     return new Promise(resolve => {
-        resolve(instance[name](model))
-      })
+      resolve(instance[name](model));
+    })
       .then(result => {
         if (result !== null && result !== undefined) {
           return result;
